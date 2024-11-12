be6ba8019dd3a7d1b212f3a097c2521dd55e1f3e
libinvb <libin.v@ltslean.com>
2024-11-12 15:20:51 +0530

    updated

--- src/app/template.js
+++ src/app/template.js
@@ -7,7 +7,7 @@ export default function Template({ children }) {
     <motion.div
       initial={{ y: 20, opacity: 0 }}
       animate={{ y: 0, opacity: 1 }}
-      transition={{ ease: "easeInOut", duration: 0.75 }}
+      transition={{ ease: "easeOut", duration: 2  }}
     >
       {children}
     </motion.div>
